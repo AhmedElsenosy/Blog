0aed44911ae6f28f766af05255a8f6217054b9b8
AhmedElsenosy <ahmedelsenosy487@gmail.com>
2025-03-18 14:57:13 +0200

    Add Profile model and user profile creation signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile (models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    age = models.IntegerField(default=0 , blank=True)
+    img = models.ImageField(upload_to='profile_img/' , height_field ='300' , width_field='300' , blank=True)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
